3578108f4585e6ed498d4e8a1a4c60e216109c6b
Juli Sah <juli@truemark.tech>
2024-02-17 12:07:03 +0545

    configure translation

--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,7 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  serverDependenciesToBundle: ["remix-i18next", "accept-language-parser"],
+  serverModuleFormat: "esm",
+  serverPlatform: "node",
 };
